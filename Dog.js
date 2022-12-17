29af1731e4f3dc087a8ab34adb05cb0c2a305092
arslonga83 <jlwood83@gmail.com>
2022-12-17 14:55:15 -0800

    get image and text from script file

--- Dog.js
+++ Dog.js
@@ -2,5 +2,16 @@ export default class Dog {
   constructor(data) {
     Object.assign(this, data)
   }
+
+  getDogHtml() {
+    const {name, avatar, age, bio} = this;
+    return `
+        <img src="${avatar}" alt="">
+        <div class="img-text">
+            <h1>${name}, ${age}</h1>
+            <p>${bio}</p>
+        </div>
+      `
+  }
   
 }
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import Dog from './Dog.js'
 import dogs from './data.js'
 
-console.log(Dog)
-console.log(dogs)
+const profile = new Dog(dogs[0])
+
+document.querySelector('main').innerHTML = profile.getDogHtml()
