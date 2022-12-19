8f3924afcb54526a872500ef7e3c94a29e904b7a
arslonga83 <jlwood83@gmail.com>
2022-12-19 15:11:55 -0800

    add profile page

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const dogsArray = dogs.map((dog) => {
   return new Dog(dog)
 })
 
+const likeBtn = document.querySelector('#like-btn')
+const nopeBtn = document.querySelector('#nope-btn')
+
 function render() {
   document.querySelector('main').innerHTML = dogsArray[0].getDogHtml()
 }
@@ -14,30 +17,49 @@ function getNextDog() {
   render()
 }
 
-const likeBtn = document.querySelector('#like-btn')
-const nopeBtn = document.querySelector('#nope-btn')
+function getProfileHtml() {
+  return `
+        <img src="./images/cat-profile.jpg" alt="">
+        <div class="img-text">
+            <h1>AnonymousDogLover</h1>
+            <p>I know how to make a dog purr.</p>
+        </div>
+  `
+}
 
-likeBtn.addEventListener('click', (e) => {
-  likeBtn.disabled = true
-  nopeBtn.disabled = true
+function toggleBtns() {
+  likeBtn.disabled = !likeBtn.disabled
+  nopeBtn.disabled = !nopeBtn.disabled
+}
+
+document.querySelector('#profile-btn').addEventListener('click', () => {
+  document.querySelector('main').innerHTML = getProfileHtml()
+  toggleBtns()
+})
+
+document.querySelector('#paw-btn').addEventListener('click', () => {
+  likeBtn.disabled = false
+  nopeBtn.disabled = false
+  render()
+})
+
+likeBtn.addEventListener('click', () => {
+  toggleBtns()
   dogsArray[0].likeDog()
   render()
   setTimeout(() => {
     getNextDog()
-    nopeBtn.disabled = false
-    likeBtn.disabled = false
+    toggleBtns()
   }, 1500)
 })
  
-nopeBtn.addEventListener('click', (e) => {
-  nopeBtn.disabled = true
-  likeBtn.disabled = true
+nopeBtn.addEventListener('click', () => {
+  toggleBtns()
   dogsArray[0].nopeDog()
   render()
   setTimeout(() => {
     getNextDog()
-    nopeBtn.disabled = false
-    likeBtn.disabled = false
+    toggleBtns()
   }, 1500)
 })
 
